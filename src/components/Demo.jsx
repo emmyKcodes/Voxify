a9d3e4b572310e009e5d2fa19640de58723a5d83
Emmanuel Kehinde <ekehinde12345@gmail.com>
2026-03-03 02:53:42 +0100

    Perf: improved runtime performance

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -39,11 +39,7 @@ const Demo = () => {
     setTimeout(() => setCopied(false), 3000);
   };
 
-  // const handleKeyDown = (e) => {
-  //   if (e.keyCode === 13) {
-  //     handleSubmit(e);
-  //   }
-  // };
+
 
   return (
     <section className="mt-16 w-full max-w-xl">
